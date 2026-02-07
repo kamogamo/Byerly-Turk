a76b03fc59553c4c4b808f7320cc6b16c30482f8
kamogamo <navycotten0523@gmail.com>
2026-02-07 20:23:56 +0900

    vscodeでの変更

--- keibaapi/src/main/java/com/example/keibaapi/repository/mapper/GIWinnerMapper.java
+++ keibaapi/src/main/java/com/example/keibaapi/repository/mapper/GIWinnerMapper.java
@@ -1,20 +1,14 @@
 package com.example.keibaapi.repository.mapper;
 
-import com.example.keibaapi.repository.entity.GIWinner;
-import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
 @Mapper
 public interface GIWinnerMapper {
 
-    @Select("SELECT * FROM GI_winner")
-    List<GIWinner> findResult();
-
-
-
-
-
+    List<String> findGIWinner(
+            Integer year,
+            String race,
+            String winner
+    );
 }
-
-
